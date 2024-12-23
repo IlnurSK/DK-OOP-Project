51bb0fbc35262114d60ecba95ebd8e0792ea3c69
Ilnur Sirazhev <code_gex@mail.ru>
2024-12-23 15:45:10 +0500

    OOP PHP/Создание класса

---
+++ 3-Create-Classes-In-OOP-PHP/includes/newclass.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+class NewClass {
+    // Свойства и Методы здесь
+    public $info = "This is some info";
+
+}
+
+// Создание экземпляра класса (объект)
+
+$object = new NewClass();
+
+var_dump($object);

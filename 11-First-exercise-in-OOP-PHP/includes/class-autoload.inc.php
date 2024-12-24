f9a560a4fa6197bda7da77787cf954a3775be4c6
Ilnur Sirazhev <code_gex@mail.ru>
2024-12-24 15:11:50 +0500

    OOP PHP/Создание веб-кальулятора используя ООП

--- 11-First-exercise-in-OOP-PHP/includes/class-autoload.inc.php
+++ 11-First-exercise-in-OOP-PHP/includes/class-autoload.inc.php
@@ -7,8 +7,15 @@ spl_autoload_register('myAutoLoader'); // встроенная функция а
 // Создаем пользовательскую функцию автозагрузки классов
 function myAutoLoader($className)
 {
-    $path = "classes/"; // переменная с путем к папке с классами
-    $extension = ".class.php"; // расширение файлов с классами
+    $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; // URL адрес сайта
+
+    if (str_contains($url, 'includes')) { // если в URL есть includes, значит мы находимся в папке includes, и путь нужно изменить
+        $path = '../classes/'; // переменная с путем к папке с классами
+    } else {
+        $path = 'classes/'; // переменная с путем к папке с классами
+    }
+
+    $extension = '.class.php'; // расширение файлов с классами
     $fullPath = $path . $className . $extension; // полный путь к файлу класса
 
     // Обработчик ошибок
